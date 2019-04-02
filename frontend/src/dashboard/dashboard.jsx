7ac764e9452b96990f17d230685ab750a99ab6fc
Daniel Oliveira <danielso2007@gmail.com>
2019-04-02 20:12:00 -0300

    feat(Parte 01): Integrando Dashboard com Redux.

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
 import ValueBox from '../common/widget/valueBox';
 import Row from '../common/layout/row';
 
-export default class Dashboard extends Component {
+const mapStateToProps = state => ({summary: state.dashboard.summary});
+
+class Dashboard extends Component {
     render() {
+
+        const { credit, debt } = this.props.summary;
+
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 0.1.0' />
                 <Content>
                     <Row>
-                        <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de Créditos'></ValueBox>
-                        <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 20' text='Total de Débitos'></ValueBox>
-                        <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Value Consolidado'></ValueBox>
+                        <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de Créditos'></ValueBox>
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de Débitos'></ValueBox>
+                        <ValueBox cols='12 4' color='blue' icon='money' value={`R$ ${credit - debt}`} text='Value Consolidado'></ValueBox>
                     </Row>
                 </Content>
             </div>
         );
     }
 }
+
+export default connect(mapStateToProps)(Dashboard);
