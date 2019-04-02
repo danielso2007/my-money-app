bd77cb2b61af7a4b4ef05409b97cc6a2ba30cc3a
Daniel Oliveira <danielso2007@gmail.com>
2019-04-02 20:44:34 -0300

    feat: Componente BillingCycle: Refatorando para Classe.

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
 
-export default props => (
-    <div>
-        <h1>Ciclo de Pagamentos</h1>
-    </div>
-);
+
+class BillingCycle extends Component {
+
+    render() {
+       return (
+        <div>
+            <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+            <Content>
+                Ciclos de components
+            </Content>
+        </div>
+       )
+    }
+
+}
+
+export default BillingCycle;
