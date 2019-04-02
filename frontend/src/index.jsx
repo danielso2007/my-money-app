ef9c3dc34b652d7936a2fc67114e61435097d800
Daniel Oliveira <danielso2007@gmail.com>
2019-04-02 20:02:48 -0300

    feat: Configurando Redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
+
 import App from "./main/app";
+import reduces from './main/reducers';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(reduces);
 
 ReactDOM.render(
-  <HashRouter><App /></HashRouter>,document.getElementById("app")
+  <HashRouter>
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </HashRouter>,
+  document.getElementById("app")
 );
