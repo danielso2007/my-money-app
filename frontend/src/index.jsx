d4c6f08d44cab5924dd83471e14b9fa1077418a3
danielso2007 <danielso2007@gmail.com>
2019-04-06 21:23:22 -0300

    feat: Melhorias na Inclusão do Ciclo de Pagamento.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,11 +7,13 @@ import reduces from './main/reducers';
 
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
+import multi from 'redux-multi';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = applyMiddleware(promise)(createStore)(reduces, devTools);
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reduces, devTools);
 
 ReactDOM.render(
   <HashRouter>
