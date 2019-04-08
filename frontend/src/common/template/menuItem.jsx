1814c95f3c235a0afc3cf0b72e2333765b12e27a
Daniel Oliveira <danielso2007@gmail.com>
2019-04-08 20:15:14 -0300

    feat: Problema de Responsividade do Menu.

--- frontend/src/common/template/menuItem.jsx
+++ frontend/src/common/template/menuItem.jsx
@@ -4,7 +4,7 @@ import { criarIdTag } from '../utils';
 export default props => (
     <li>
         <a id={criarIdTag(`menu_${props.label}`)} href={props.path}>
-            <i className={`fa fa-${props.icon}`}></i> {props.label}
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
         </a>
     </li>
 );
--- frontend/src/common/template/menuTree.jsx
+++ frontend/src/common/template/menuTree.jsx
@@ -4,7 +4,7 @@ import { criarIdTag } from '../utils';
 export default props => (
     <li className="treeview">
         <a id={criarIdTag(`tree_menu_link_${props.label}`)} href='#'>
-            <i className={`fa fa-${props.icon}`}></i> {props.label}
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
             <span className="pull-right-container">
               <i className="fa fa-angle-left pull-right"></i>
             </span>
